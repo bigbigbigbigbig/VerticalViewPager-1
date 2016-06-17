3fa65b66365df2c1f035b3ccb1a9c499a17c7037
吾心无待 <1101202419@qq.com>
2016-06-17 17:02:00 +0800

    2016-6-17 第一次提交代码 1.0.0.0

---
+++ README.md
@@ -0,0 +1,25 @@
+# VerticalViewPager
+
+
+##Usage
+___
+###Maven
+    <dependency>
+      <groupId>com.wuxinwudai</groupId>
+      <artifactId>vvp</artifactId>
+      <version>1.0.0.0</version>
+      <type>pom</type>
+    </dependency>
+
+
+###Gradle
+    dependencies {
+      compile 'com.wuxinwudai:vvp:1.0.0.0'
+    }
+
+###lvy
+    <dependency org='com.wuxinwudai' name='vvp' rev='1.0.0.0'>
+      <artifact name='$AID' ext='pom'></artifact>
+    </dependency>
+
+
---
+++ app/src/main/java/com/wuxinwudai/vvp/sample/MainActivity.java
@@ -0,0 +1,53 @@
+package com.wuxinwudai.vvp.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.wuxinwudai.vvp.PagerAdapter;
+import com.wuxinwudai.vvp.VerticalViewPager;
+
+public class MainActivity extends AppCompatActivity {
+    private int[] resources = new int[]{
+            R.drawable.guide_brown,
+            R.drawable.guide_grey,
+            R.drawable.guide_light_green,
+            R.drawable.guide_pink,
+            R.drawable.guide_yellow
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VerticalViewPager vvp = (VerticalViewPager)findViewById(R.id.vvp);
+        vvp.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return resources.length;
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                ImageView iv = new ImageView(MainActivity.this);
+                iv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+                iv.setScaleType(ImageView.ScaleType.FIT_XY);
+                iv.setImageResource(resources[position]);
+                ((VerticalViewPager) container).addView(iv);
+                return iv;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                ((VerticalViewPager) container).removeView((View) object);
+            }
+        });
+    }
+}
